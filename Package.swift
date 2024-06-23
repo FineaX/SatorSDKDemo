fdc268764327a3e8c621008116d26628249bd5b1
FineaX <comsubins@gmail.com>
2024-06-23 17:16:45 +0800

    1

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
+//.package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
+//.package(url: "https://github.com/CoderMJLee/MJRefresh.git", from: "3.7.1"),
+//.package(url: "https://github.com/jdg/MBProgressHUD.git", .upToNextMajor(from: "1.2.0"))
 import PackageDescription
 
 let package = Package(
@@ -11,12 +13,7 @@ let package = Package(
     products: [
         .library(name: "SatorSDK", targets: ["SatorSDK"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
-        .package(url: "https://github.com/CoderMJLee/MJRefresh.git", from: "3.7.1"),
-        .package(url: "https://github.com/jdg/MBProgressHUD.git", .upToNextMajor(from: "1.2.0"))
-
-    ],
+    dependencies: [],
     targets: [
         .binaryTarget(
             name: "SatorSDK",
